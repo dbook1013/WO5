c7c770306896953ffb9a4fd64e05ee876e9e083d
dbook1013 <144655596+dbook1013@users.noreply.github.com>
2023-10-16 13:07:58 +0900

    Add files via upload

--- master.js
+++ master.js
@@ -1,33 +1,33 @@
 var Body = {
-  setColor: function(color){
-    document.querySelector('body').style.color=color;  
+  setBackgroundColor : function(color){
+    document.querySelector('body').style.backgroundColor = color;
   },
-  setBackgroundColor: function(color){
-    document.querySelector('body').style.backgroundColor=color;  
+  setColor : function(color){
+    document.querySelector('body').style.color = color;
   }
 }
-
 var Links = {
-  setColor: function(color){
-    var alist = document.querySelectorAll('a');
+  setColor : function(color){
     var i = 0;
-    while (i < alist.length) {
+    var alist = document.querySelectorAll('a');
+    while (i < alist.length){
       alist[i].style.color = color;
       i = i + 1;
-    }
+    } 
   }
 }
 
 function nightDayHandler(self){
-  if(self.value == 'night'){
+  var target = document.querySelector('body');
+  if(self.value=='night'){
     Body.setBackgroundColor('black');
-    Body.setColor('white');   
-    Links.setColor('powderblue')   
-    self.value = 'day';
+    Body.setColor('white');
+    Links.setColor('powderblue'); 
+    self.value='day';
   } else {
-    Body.setBackgroundColor('white')
-    Body.setColor('black');   
+    Body.setBackgroundColor('white');
+    Body.setColor('black');        
     Links.setColor('blue');
-    self.value = 'night';  
-  } 
-}
+    self.value='night';
+  }
+} 
